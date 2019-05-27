567256346e9f73efc67c0c1f7bfd5b7906344f3a
Sam Tancharoensuksavai <stancharoen@gmail.com>
2019-05-27 14:54:50 -0500

    Set initial personal component style

--- src/components/personal.js
+++ src/components/personal.js
@@ -1,5 +1,25 @@
 import React from "react";
 
 export default function Personal() {
-  return <div className="duck">Hello</div>;
+  return (
+    <div className="container">
+      <div className="name">Sam Stone</div>
+      <div className="title">Software Developer</div>
+      <div className="metalinks">[bloop]</div>
+      <div className="contact">
+        <div className="contact-subcontainer">
+          <div className="icon" />
+          <div className="contact-info">myemail@gmail.com</div>
+        </div>
+        <div className="contact-subcontainer">
+          <div className="icon" />
+          <div className="contact-info">+1 (800) 555-1234</div>
+        </div>
+        <div className="contact-subcontainer">
+          <div className="icon" />
+          <div className="contact-info">Austin, TX</div>
+        </div>
+      </div>
+    </div>
+  );
 }
